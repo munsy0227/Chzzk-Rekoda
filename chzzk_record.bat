febe98306d094f9c23e2e2e475059475ad48f98e
munsy0227 <munsy0227@gmail.com>
2025-01-29 13:50:18 +0900

    pip에서 uv로 전환

--- chzzk_record.bat
+++ chzzk_record.bat
@@ -1,32 +1,9 @@
 @echo off
-echo Checking for an installed Python version...
-
-REM Check for Python 3 and capture the version output
-for /f "delims=" %%i in ('python --version 2^>^&1') do set pyversion=%%i
-
-REM Check if the version string contains "Python 3"
-echo %pyversion% | findstr /C:"Python 3" > nul
-if %errorlevel% == 0 (
-    echo Found installed Python: %pyversion%
-) else (
-    echo Python 3 is not installed. Please install Python 3.
-    exit /b 1
-)
-
-REM Detect the current directory and activate the virtual environment
-set VENV_DIR=%CD%\venv
-if exist "%VENV_DIR%" (
-    echo Activating virtual environment in %VENV_DIR%
-    call "%VENV_DIR%\Scripts\activate"
-) else (
-    echo Virtual environment not found in %VENV_DIR%
-    exit /b 1
-)
 
 REM Set the environment variable to force UTF-8 encoding
 set PYTHONUTF8=1
 
 REM Execute your Python script
-python chzzk_record.py
+uv run chzzk_record.py
 
 exit /b 0
--- settings.bat
+++ settings.bat
@@ -1,36 +1,11 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-echo Checking for an installed Python version...
-
-REM Check for Python 3 and capture the version output
-for /f "delims=" %%i in ('python --version 2^>^&1') do set pyversion=%%i
-
-REM Check if the version string contains "Python 3"
-echo !pyversion! | findstr /C:"Python 3" > nul
-if %errorlevel% == 0 (
-    echo Found installed Python: !pyversion!
-    set py=python
-) else (
-    REM If Python 3 is not found, try finding python3 explicitly
-    where python3 >nul 2>&1
-    if %errorlevel% == 0 (
-        set py=python3
-        echo Using python3 as fallback
-    ) else (
-        echo Python 3 is not installed. Please install Python 3.
-        exit /b 1
-    )
-)
-
-echo Activating the virtual environment...
-call venv\Scripts\activate
-
 REM Set the environment variable to force UTF-8 encoding
 set PYTHONUTF8=1
 
 echo Executing the settings script...
-!py! settings.py
+uv run settings.py
 
 echo Script execution completed.
 exit /b 0
